c8260a4ebe6c503f9fe449ff06adc29accb2deac
Lawrence <lawrencesalinas@yahoo.com>
2022-02-17 15:15:12 -0500

    created the boarding and working with indexes

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,2 +1,11 @@
+# create a display board for the game
 def display_board(board):
-    pass
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('-|-|-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-|-|-')
+    print(board[0] + '|' + board[1] + '|' + board[2] )
+    
+    
+test_board = ['#','X','O','X','X','O','X','X','O','X']
+display_board(test_board)
